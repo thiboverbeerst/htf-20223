1f1974b5f310c8f6fa4469e00f991cde9670331c
niels.soete <niels.soete@student.howest.be>
2023-11-09 09:51:20 +0100

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template>
+    <div>
+        <h1>Header</h1>
+    </div>
+</template>
+
+<style lang="css">
+    .h1 {
+        color: red;
+    }
+</style>
